fec6daa28f2e979d0a4739fba8da8121cd6c739a
hayatulfarah <47801141+hayatulfarah@users.noreply.github.com>
2021-02-17 11:55:56 +0800

    Add files via upload

---
+++ storage/framework/views/0d9241f51176127f513b15bd03151c42dbdacdb7.php
@@ -0,0 +1,7 @@
+
+
+<?php $__env->startSection('content'); ?>
+WELCOME PAGE
+
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\Asus\webapp\resources\views/posts/index.blade.php ENDPATH**/ ?>
